ef2aa00142875e2198abaf27afb8669fda847571
Your NameTruongNgocTinhAnh <anhtruong1012@gmail.com>
2016-07-20 13:33:57 +0700

    b1

--- Bai1_AnhTruong/src/bai1_anhtruong/Bai1_AnhTruong.java
+++ Bai1_AnhTruong/src/bai1_anhtruong/Bai1_AnhTruong.java
@@ -15,7 +15,11 @@ public class Bai1_AnhTruong {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("**  **  ******  **      **       ****");
+        System.out.println("**  **  **      **      **      **  **");
+        System.out.println("******  ******  **      **      **  **");
+        System.out.println("**  **  **      **      **      **  **");
+        System.out.println("**  **  ******  ******  ******   ****");
     }
     
 }
